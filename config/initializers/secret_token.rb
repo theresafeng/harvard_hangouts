6b70c08c29dfa7368b2a493227873d8352e39d34
Theresa <theresa.feng@college.harvard.edu>
2012-11-19 23:10:41 -0500

    init comimit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HarvardHangouts::Application.config.secret_token = 'ba6426b61fc8eeead306ac4967e745697d799dd298ee676463f959a3d791d9b67895b58ec23940b280caeb7d6c0e6ac90813faf2ca18a8845d61fa4cd62aa2e7'
