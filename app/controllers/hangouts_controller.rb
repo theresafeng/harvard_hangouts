bb8d7ece882d78771503376df49d958b4e226980
Theresa <theresa.feng@college.harvard.edu>
2012-11-25 00:42:05 -0500

    save hangout

--- app/controllers/hangouts_controller.rb
+++ app/controllers/hangouts_controller.rb
@@ -2,4 +2,10 @@ class HangoutsController < ApplicationController
   def new
     @hangout = Hangout.new
   end
+  
+  def create
+    Hangout.create(params[:hangout])
+    redirect_to root_path
+  end
+  
 end
