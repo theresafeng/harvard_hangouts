101fe85d0a8ffab01cf922bc8d9996a4fe13e1a3
Theresa <theresa.feng@college.harvard.edu>
2012-11-25 20:39:59 -0500

    validate hangout form

--- app/models/hangout.rb
+++ app/models/hangout.rb
@@ -1,3 +1,6 @@
 class Hangout < ActiveRecord::Base
   attr_accessible :description, :end_date_time, :event, :location, :name, :start_date_time
+
+  validates :description, :presence => true, length: {maximum: 100}  
+  validates_presence_of :name, :event, :location, :start_date_time, :end_date_time
 end
