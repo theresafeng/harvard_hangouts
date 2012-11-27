d8186e07dd4eff877b7045731868eb3fe426c864
Theresa <theresa.feng@college.harvard.edu>
2012-11-27 00:17:25 -0500

    add username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121127042855) do
+ActiveRecord::Schema.define(:version => 20121127050555) do
 
   create_table "hangouts", :force => true do |t|
     t.string   "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20121127042855) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
