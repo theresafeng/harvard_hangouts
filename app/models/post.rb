7288f8e0d38077a6e2c818650d464e1b76e551ba
Theresa <theresa.feng@college.harvard.edu>
2012-11-26 23:51:31 -0500

    associate user to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
   attr_accessible :description, :end_date_time, :event, :location, :start_date_time
   validates :description, :presence => true, length: {maximum: 100}  
   validates_presence_of :event, :location, :start_date_time, :end_date_time
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126020514) do
+ActiveRecord::Schema.define(:version => 20121127042855) do
 
   create_table "hangouts", :force => true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20121126020514) do
     t.datetime "end_date_time"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
