4f12bf4d7d1033c7e2e64343dcae2b6b6ca87846
Dennis Lu <Dennis@Dennis-Lus-MacBook-Pro.local>
2012-12-08 05:21:02 -0500

    added super sexy about page, redirects non-users to about page, added links to user profiles in comments and individual post views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,16 +4,16 @@ class WelcomeController < ApplicationController
     @hangouts = Hangout.order("start_date_time DESC").page(params[:page]).per(10)
   end
   def work
-    @hangouts = Hangout.order("start_date_time DESC")
+    @hangouts = Hangout.where("event = 'Work'").order("start_date_time DESC")
   end
   def sessions
-    @hangouts = Hangout.order("start_date_time DESC")
+    @hangouts = Hangout.where("event = 'Session'").order("start_date_time DESC")
   end
   def parties
-    @hangouts = Hangout.order("start_date_time DESC")
+    @hangouts = Hangout.where("event = 'Party'").order("start_date_time DESC")
   end
   def other
-    @hangouts = Hangout.order("start_date_time DESC")
+    @hangouts = Hangout.where("event = 'Other'").order("start_date_time DESC")
   end
   def about
   end
